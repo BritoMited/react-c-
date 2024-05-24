1b13a5a255fa9cbeb870c4e2b1ddc02ef9428e7b
Auto Logon <autologon@CSED.LAB>
2024-05-24 11:46:40 -0300

    criaçao de projeto de consulta de cep

---
+++ other-proj/my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import ConsultaCEP from './components/pages/cep/ConsultaCEP';
+
+function App() {
+
+  return (
+    <div>
+      <h1>kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk</h1>
+      <ConsultaCEP></ConsultaCEP>
+    </div>
+  );
+}
+
+export default App;
