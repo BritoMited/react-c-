db76d3481c9e857c2ffeb5e25583463b290e2ae3
Auto Logon <autologon@CSED.LAB>
2024-06-07 11:29:05 -0300

    criar consultas cep

---
+++ other-proj/my-app/src/components/pages/cep/cep-consulta.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react"
+import { Endereco } from "../../../models/Endereco"
+
+// EXERCICIOS
+//1 - consulta api (CORS)
+//2 - EXIBIR NO HTML A LISTA DE PRODUTOS
+
+function CepConsultar(){
+  //Evento de carregamento do componente
+      const [rua, setRua] = useState("");
+      const [bairro, setBairro] = useState("");
+      const [cidade, setCidade] = useState("");
+      const [cep, setCep] = useState("");
+      useEffect(() => {
+  
+      }, []);
+  
+  
+      function carregarCep(){
+          //Fetch ou axios
+          //fetch == buscar
+          fetch("https://viacep.com.br/ws/"+ cep +"/json/")
+          .then((resposta) =>
+              resposta.json()
+          )
+          .then((endereco: Endereco) => {
+              setRua(endereco.logradouro);
+              setBairro(endereco.bairro);
+              setCidade(endereco.localidade);          
+          });
+      }
+      return(
+          
+      
+          <div>
+              <h1>Consultar CEP</h1>
+             <input 
+             type="text"
+             placeholder="Digite seu CEP"
+             onBlur={carregarCep}
+             onChange={
+              (e : any) => setCep(e.target.value)
+             }
+      
+          />
+              
+  
+              <p>Rua {rua}</p>
+              <button> vc mora no bairro: {bairro}</button>
+              <input type="text" disabled value={cidade}/>
+          </div>
+  
+         
+      )
+  }
+
+export default CepConsultar;
